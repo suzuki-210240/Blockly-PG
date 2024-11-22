58f7d297283cfc7926e113d1c85f53674455cd58
Suzuki <tejc210240@gmail.com>
2024-11-22 13:11:35 +0900

    更新

--- Blockly/settings.py
+++ Blockly/settings.py
@@ -122,3 +122,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# CSRFの設定
+CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1:8000', 'http://localhost:8000']
