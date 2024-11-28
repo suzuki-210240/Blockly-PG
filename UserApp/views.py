1a06e6b616261f87dd4ffaba4a1de68be0d1c8de
test <tejc210240@gmail.com>
2024-11-28 13:00:48 +0900

    更新・追加

--- UserApp/views.py
+++ UserApp/views.py
@@ -1,3 +1,13 @@
+import re
+from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render
+from django.utils.timezone import datetime
+from django.http import HttpResponse
+from django.http import JsonResponse
+import json
 
-# Create your views here.
+def Home (request):
+    return render(
+        request,
+        'main.html',
+    )
