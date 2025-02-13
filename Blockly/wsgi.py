cc158cf9dabc784a1cb8755175afacc10072a7db
test user <tejc210240@gmail.com>
2025-02-13 04:27:41 +0000

    更新

--- Blockly/wsgi.py
+++ Blockly/wsgi.py
@@ -1,18 +1,9 @@
-"""
-WSGI config for Blockly project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
-"""
-
 import os
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise import WhiteNoise
+from whitenoise import WhiteNoise  # 注意: WhiteNoise と大文字でインポート
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Blockly.settings')
 
 application = get_wsgi_application()
-application = whitenoise(application,root="/home/ec2-user/Blockly-PG/staticfiles")
+application = WhiteNoise(application, root="/home/ec2-user/Blockly-PG/staticfiles")  # WhiteNoise を使う
